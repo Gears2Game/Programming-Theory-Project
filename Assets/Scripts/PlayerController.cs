a02765149dd8becebd1ebb0dff5229f6336a50f8
khai014 <70681434+khai014@users.noreply.github.com>
2022-04-01 08:09:37 +0800

    Ongoin Work on MainGameScene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _projejctile;
+	[SerializeField] private float _projectileForce;
+    [SerializeField] private float _speed;
+
+	private Rigidbody _playerRb;
+	private bool _isOnGround;
+	private float _zBoundary = 18;
+
+    void Start()
+    {
+        _playerRb = GetComponent<Rigidbody>();    
+    }
+
+	void Update()
+	{
+		MovePlayerOnGround();
+		Attack();
+		PreventMovementOnBoundary();
+	}
+
+	private void PreventMovementOnBoundary()
+	{
+		if (transform.position.z > _zBoundary)
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y, _zBoundary);
+		}
+
+		if (transform.position.z < -_zBoundary)
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y, -_zBoundary);
+		}
+	}
+
+	private void Attack()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			_projejctile.AddForce(Vector3.forward * _projectileForce, ForceMode.Impulse);
+		}
+	}
+
+	private void MovePlayerOnGround()
+	{
+		float setHorizontalInput = Input.GetAxis("Horizontal");
+		float setVerticalInput = Input.GetAxis("Vertical");
+
+		if (_isOnGround)
+		{
+			_playerRb.AddForce(Vector3.forward * _speed * setVerticalInput);
+			_playerRb.AddForce(Vector3.right * _speed * setHorizontalInput);
+		}
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.CompareTag("Ground"))
+		{
+			_isOnGround = true;
+		}
+
+		if (collision.gameObject.CompareTag("Enemy"))
+		{
+			Debug.Log("Collided with the enemy");
+		}
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("PowerUp"))
+		{
+			Destroy(other.gameObject);
+		}
+	}
+}
